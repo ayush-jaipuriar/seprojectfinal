33a44709237ca965b9a955132ddb415d6819dde6
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2019-11-03 15:48:08 +0530

    "Added a lot of boiler plate code to request storage permission from the user and implemented it for different build versions as some old build versions did not require an explicit permission"

--- app/src/main/java/com/example/seprojectfinal/socialMediaActivity.java
+++ app/src/main/java/com/example/seprojectfinal/socialMediaActivity.java
@@ -1,10 +1,20 @@
 package com.example.seprojectfinal;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,6 +29,7 @@ public class socialMediaActivity extends AppCompatActivity {
     private ImageView postImageView;
     private Button btnCreatePost;
     private ListView usersListView;
+    private Bitmap bitmap;
 
 
     @Override
@@ -35,6 +46,7 @@ public class socialMediaActivity extends AppCompatActivity {
         postImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                selectImage();
 
             }
         });
@@ -77,4 +89,50 @@ public class socialMediaActivity extends AppCompatActivity {
         finish();
 
     }
+
+    private  void selectImage() {
+        if (Build.VERSION.SDK_INT < 23) {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent,1000);
+        }
+        else if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+            PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+
+            } else {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,100);
+            }
+
+        }
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1000 && grantResults[0]== PackageManager.PERMISSION_GRANTED) {
+            selectImage();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000 && resultCode == RESULT_OK && data != null) {
+            Uri chosenImageData = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), chosenImageData);
+                postImageView.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
 }
