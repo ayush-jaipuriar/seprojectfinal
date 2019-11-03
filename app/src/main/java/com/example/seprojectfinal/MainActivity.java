16adb50ac5f4419e561ea2d6edc549dc28aa62fe
Ayush Jaipuriar <jaipuriar.ayush@gmail.com>
2019-11-03 13:31:01 +0530

    "Added and initialized the views and will now implement the login(authentication) implementation in the comming commits"

--- app/src/main/java/com/example/seprojectfinal/MainActivity.java
+++ app/src/main/java/com/example/seprojectfinal/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.seprojectfinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtEmail, edtUsername, edtPassword;
+    Button btnSignUP, btnSignIn;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUP = findViewById(R.id.btnSignUp);
+        mAuth = FirebaseAuth.getInstance();
     }
 }
